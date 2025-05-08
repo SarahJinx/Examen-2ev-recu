a159b65053207985d87f0651675af8c2a681d742
Sarah <scartleha1123@gmail.com>
2025-05-08 18:42:04 +0200

    Apartado 5

--- Examen2Eval (alumns)/Assets/Scripts/FlyingEnemy.cs
+++ Examen2Eval (alumns)/Assets/Scripts/FlyingEnemy.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class FlyingEnemy : MonoBehaviour
 {
+    private float maxTime = 5, currTime = 0;
+    public float xSpeed = 1f, ySpeed = 2f;
+    private Vector2 dir = Vector2.right;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-       
+        transform.Translate(new Vector2(xSpeed, Mathf.Sin(Time.time) * ySpeed) * Time.deltaTime);
+        currTime += Time.deltaTime;
+        if (currTime >= maxTime)
+        {
+            xSpeed *= -1;
+            currTime = 0;
+        }
     }
 }
